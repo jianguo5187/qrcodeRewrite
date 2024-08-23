f2e48b26469297ecffefd3ace446b52e1fc31cee
jianguo <jianguo5187@gmail.com>
2024-08-23 23:03:49 +0800

    0823（跳转增加）

--- src/main/java/com/qrcode/rewrite/controller/AppController.java
+++ src/main/java/com/qrcode/rewrite/controller/AppController.java
@@ -1,16 +1,16 @@
 package com.qrcode.rewrite.controller;
 
 import com.qrcode.rewrite.dto.AjaxResult;
-import com.qrcode.rewrite.dto.UpdateEntryUrlReqVO;
-import com.qrcode.rewrite.dto.UpdateMainUrlReqVO;
 import com.qrcode.rewrite.pojo.SysEntryDomain;
 import com.qrcode.rewrite.pojo.SysMainDomain;
 import com.qrcode.rewrite.service.SysEntryDomainService;
 import com.qrcode.rewrite.service.SysMainDomainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/app")
--- src/main/java/com/qrcode/rewrite/controller/RedirectController.java
+++ src/main/java/com/qrcode/rewrite/controller/RedirectController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 @Controller
 public class RedirectController {
 
@@ -14,37 +18,91 @@ public class RedirectController {
     private SysMainDomainService mainDomainService;
 
     @GetMapping("/")
-    public ModelAndView redirectBasedOnQueryParam(@RequestParam(name = "webType", required = false) String webType,@RequestParam(name = "parentUserId", required = false) Long parentUserId) {
+    public ModelAndView redirectBasedOnQueryParam(HttpServletRequest request, @RequestParam(name = "webType", required = false) String webType, @RequestParam(name = "parentUserId", required = false) Long parentUserId) throws InterruptedException {
 
         if (webType != null && !webType.isEmpty()) {
 
             String redirectUrl = mainDomainService.getMainUrlByWebType(webType); // 可能需要处理为完整的URL，例如添加协议或路径
             if(!StringUtils.isEmpty(redirectUrl)){
-                if(!redirectUrl.startsWith("http") && !redirectUrl.startsWith("https")){
-                    redirectUrl = "http://" + redirectUrl;
-                }
+                String redirectHostUrl = request.getRequestURL().toString();
+                redirectHostUrl = redirectHostUrl + "/dpc/qrRedirect?redirectUrl=" + urlEncode(redirectUrl);
+
                 if(parentUserId != null && parentUserId > 0){
-                    redirectUrl = redirectUrl + "?parentUserId=" + parentUserId;
+                    redirectHostUrl = redirectHostUrl + "&parentUserId=" + parentUserId;
                 }
-                return new ModelAndView("redirect:" + redirectUrl);
+                return new ModelAndView("redirect:" + redirectHostUrl);
+
+//                Thread.sleep(1000);//延迟1秒跳转
+//                if(!redirectUrl.startsWith("http") && !redirectUrl.startsWith("https")){
+//                    redirectUrl = "http://" + redirectUrl;
+//                }
+//                if(parentUserId != null && parentUserId > 0){
+//                    redirectUrl = redirectUrl + "?parentUserId=" + parentUserId;
+//                }
+//                return new ModelAndView("redirect:" + redirectUrl);
             }else{
 
                 // 如果没有提供rewrite参数，或者参数为空，可以重定向到默认页面
-                return new ModelAndView("redirect:/defaultPage");
+                return new ModelAndView("redirect:http://www.baidu.com");
             }
-//        }
-//
-//        if (rewriteUrl != null && !rewriteUrl.isEmpty()) {
-//            // 这里假设rewriteUrl已经是完整的URL，包括协议（http://或https://）
-//            // 如果rewriteUrl只是域名或路径，你需要根据需要进行拼接
-//            String redirectUrl = rewriteUrl; // 可能需要处理为完整的URL，例如添加协议或路径
-//            if(!redirectUrl.startsWith("http") || !redirectUrl.startsWith("https")){
-//                redirectUrl = "http://" + redirectUrl;
-//            }
-//            return new ModelAndView("redirect:" + redirectUrl);
         } else {
             // 如果没有提供rewrite参数，或者参数为空，可以重定向到默认页面
-            return new ModelAndView("redirect:/defaultPage");
+            return new ModelAndView("redirect:http://www.baidu.com");
+        }
+    }
+
+
+    @GetMapping("/dpc/qrRedirect")
+    public ModelAndView qrRedirect(HttpServletRequest request, @RequestParam(name = "redirectUrl", required = false) String redirectUrl, @RequestParam(name = "parentUserId", required = false) Long parentUserId) {
+
+        String redirectHostUrl = request.getRequestURL().toString().replace(request.getRequestURI(),"");
+        redirectHostUrl = redirectHostUrl + "/dpc/qrRedirectSecond?redirectUrl=" + urlEncode(redirectUrl);
+        if(parentUserId != null && parentUserId > 0){
+            redirectHostUrl = redirectHostUrl + "&parentUserId=" + parentUserId;
+        }
+        return new ModelAndView("redirect:" + redirectHostUrl);
+    }
+
+    @GetMapping("/dpc/qrRedirectSecond")
+    public ModelAndView qrRedirectSecond(HttpServletRequest request, @RequestParam(name = "redirectUrl", required = false) String redirectUrl, @RequestParam(name = "parentUserId", required = false) Long parentUserId) {
+
+        String redirectHostUrl = request.getRequestURL().toString().replace(request.getRequestURI(),"");
+        redirectHostUrl = redirectHostUrl + "/dpc/threeRedirect?redirectUrl=" + urlEncode(redirectUrl);
+        if(parentUserId != null && parentUserId > 0){
+            redirectHostUrl = redirectHostUrl + "&parentUserId=" + parentUserId;
+        }
+        return new ModelAndView("redirect:" + redirectHostUrl);
+    }
+
+    @GetMapping("/dpc/threeRedirect")
+    public ModelAndView threeRedirect(@RequestParam(name = "redirectUrl", required = false) String redirectUrl, @RequestParam(name = "parentUserId", required = false) Long parentUserId) throws InterruptedException {
+        Thread.sleep(1000);//延迟1秒跳转
+
+        if(!redirectUrl.startsWith("http") && !redirectUrl.startsWith("https")){
+            redirectUrl = "http://" + redirectUrl;
+        }
+        if(parentUserId != null && parentUserId > 0){
+            redirectUrl = redirectUrl + "?parentUserId=" + parentUserId;
+        }
+        return new ModelAndView("redirect:" + redirectUrl);
+    }
+
+
+    /**
+     * 内容编码
+     *
+     * @param str 内容
+     * @return 编码后的内容
+     */
+    public static String urlEncode(String str)
+    {
+        try
+        {
+            return URLEncoder.encode(str, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            return "";
         }
     }
 }
